b1ee7ee69678bca961abc5e5a56d50f1c269ebd7
Tian Yang <tyang2896@conestogac.on.ca>
2024-10-04 15:01:51 -0400

    Added data section and modified add_test

--- tyang2896_asm.s
+++ tyang2896_asm.s
@@ -3,6 +3,21 @@
 @ This is a comment. Anything after an @ symbol is ignored.
 @@ This is also a comment. Some people use double @@ symbols. 
 
+@ tyang2896_asm.s Data section - initialized values
+    .data
+
+    .align 3    @ This alignment is critical - to access our "huge" value, it must
+                @ be 64 bit aligned
+
+    huge:   .octa 0xAABBCCDDDDCCBBFF
+    big:    .word 0xAAEEBBFF
+    num:    .byte 0xAB
+
+
+    str2:   .asciz "Guten Tag!"
+    count:  .word 12345                     @ This is an initialized 32 bit value
+
+@ End of new data section
 
     .code   16              @ This directive selects the instruction set being generated. 
                             @ The value 16 selects Thumb, with the value 32 selecting ARM.
@@ -33,6 +48,26 @@
 
 @ Here is the actual tyang2896_add_test function
 tyang2896_add_test:
+    @ Load the addresses of each of our items
+    ldr r0, =num
+    ldr r0, =big
+    ldr r0, =huge
+    ldr r0, =str2
+
+    ldr r2, =str2			@ Load the address of str2 and store it in r2
+    ldrb r0, [r2]			@ Load the value stored at the address str2 as a byte
+
+    ldr r2, =str2			@ Load the address of str2 and store it in r2
+    ldr r0, [r2]			@ Load the value stored at the address str2 as a word
+
+    ldr r2, =num			@ Load the address of num and store it in r2
+    ldrb r0, [r2]			@ Load the value stored at the address num
+
+    ldr r2, =big			@ Load the address of big
+    ldr r0, [r2]			@ Load the value of big
+
+    ldr r2, =huge			@ Load the address of huge
+    ldrd r0, r1, [r2]		@ Load the value of huge
 
     add r0, r0, r1
 
