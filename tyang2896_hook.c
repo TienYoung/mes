a78859bf2519dda5e2b77b16e0561b97c91b2c48
Tian Yang <tyang2896@conestogac.on.ca>
2024-10-05 23:06:35 -0400

    Added tyang2896_a2 prototype

--- tyang2896_hook.c
+++ tyang2896_hook.c
@@ -10,6 +10,7 @@
 #include "common.h"
 
 int tyang2896_add_test(int x, int y, int delay);
+int tyang2896_a2(int num, int wait);
 
 void AddTest(int action)
 {
@@ -40,3 +41,41 @@ void AddTest(int action)
 }
 
 ADD_CMD("tyang2896_add", AddTest,"Test the new add function")
+
+// Assignment 2 C Hook Function
+//
+void _tyang2896_Assignment2(int action)
+{
+
+  if(action==CMD_SHORT_HELP) return;
+  if(action==CMD_LONG_HELP) {
+    printf("Assignment 2\n\n"
+	   "This command triggers assignment 2 by tyang2896\n"
+	   );
+
+    return;
+  }
+
+  // Retrieve user inputs for count and delay here
+  uint32_t count;
+  uint32_t delay;
+  int fetch_status;
+
+  fetch_status = fetch_uint32_arg(&count);
+
+  if(fetch_status) {
+  	// Use a default value
+  	count = 0xFFFFEF;
+  }
+
+  fetch_status = fetch_uint32_arg(&delay);
+
+  if(fetch_status) {
+  	// Use a default value
+  	delay = 0xFFFFEF;
+  }
+
+  printf("tyang2896_a2 returned: %d\n", tyang2896_a2(count, delay));
+}
+
+ADD_CMD("tyang2896_a2", _tyang2896_Assignment2, "Assignment 2")
